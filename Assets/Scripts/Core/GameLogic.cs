d091453d2e734cd89ebad37194c358a514257056
Jeongjin Oh <onsemy@gmail.com>
2020-01-13 19:00:09 +0900

    기초적인 플로우 구성(중)
    * 1회 플레이가 가능하도록 작업중입니다.
    * Start->Play->(Result)->Start 중에서 Result는 현재 없습니다 (없을 예정?)
    * 장애물이 화면을 벗어난 후 재생성되는 로직을 작업중입니다 (현재 동작X)

--- Assets/Scripts/Core/GameLogic.cs
+++ Assets/Scripts/Core/GameLogic.cs
@@ -1,33 +1,70 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using JJFramework.Runtime;
 using JJFramework.Runtime.Attribute;
 using JJFramework.Runtime.Extension;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace GameRuntime.Core.Play
 {
-    public class GameLogic : ExMonoBehaviour
+    public class GameLogic : ExMonoSingleton<GameLogic>
     {
-        [ComponentPath] protected Text txtScore;
-        [ComponentPath] protected Player objPlayer;
+        [ComponentPath, SerializeField] protected Button btnStart;
+        [ComponentPath, SerializeField] protected Text txtTime;
+        [ComponentPath, SerializeField] protected Player objPlayer;
+        [ComponentPath, SerializeField] protected BoxCollider objCube;
+        [ComponentPath, SerializeField] protected BoxCollider objRegenArea;
+
+        [SerializeField] private FloatReactiveProperty _time = new FloatReactiveProperty(0f);
+
+        public BoolReactiveProperty IsStart { get; } = new BoolReactiveProperty(false);
 
         protected override void Awake()
         {
             base.Awake();
-        }
 
-        // Start is called before the first frame update
-        void Start()
-        {
+            IsStart.Subscribe(x => btnStart.SetActive(!x));
+            IsStart.Value = false;
+            
+            txtTime.text = string.Empty;
 
-        }
+            objCube.UpdateAsObservable()
+                .Select(x => objCube.transform)
+                .Where(t => IsStart.Value)
+                .Subscribe(t => t.Translate(-Time.deltaTime * 5f, 0f, 0f));
 
-        // Update is called once per frame
-        void Update()
-        {
+            objCube.OnTriggerEnterAsObservable()
+                .Where(c => c.name == nameof(objPlayer))
+                .Subscribe(c => IsStart.Value = false);
+            
+            objRegenArea.OnTriggerEnterAsObservable()
+                .Where(c => c.name == nameof(objCube))
+                .Subscribe(c => objCube.transform.localPosition = new Vector3(4f, -1.5f, 0f));
 
+            Observable.EveryUpdate()
+                .Where(x => IsStart.Value)
+                .Where(x => Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                .Subscribe(_ => objPlayer.Jump());
+
+            btnStart.OnClickAsObservable()
+                .Where(x => IsStart.Value == false)
+                .Subscribe(_ =>
+                {
+                    objCube.transform.localPosition = new Vector3(4f, -1.5f, 0f);                   
+                    _time.Value = 0f;
+                    IsStart.Value = true;
+                });
+
+            Observable.EveryUpdate()
+                .Where(x => IsStart.Value)
+                .Subscribe(_ => _time.Value += Time.deltaTime);
+
+            _time.SubscribeToText(txtTime, x => $"Time: {x:0.0}");
         }
     }
 }
--- Assets/Scripts/Core/Player.cs
+++ Assets/Scripts/Core/Player.cs
@@ -1,27 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using JJFramework.Runtime.Extension;
+using UniRx;
 using UnityEngine;
 
 namespace GameRuntime.Core.Play
 {
     public class Player : ExMonoBehaviour
     {
+        [SerializeField] private CharacterController _controller;
+        [SerializeField] private float _jumpFactor = 10f;
+        [SerializeField] private Vector3 _direction;
+        [SerializeField] private float _targetHeight;
+
+        private const float _gravity = 9.8f * 2f;
+
         protected override void Awake()
         {
             base.Awake();
+
+            _controller = GetComponent<CharacterController>();
+
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                if (_controller.isGrounded == false)
+                {
+                    _direction.y -= _gravity * Time.deltaTime;
+                }
+
+                _controller.Move(_direction * Time.deltaTime); // * Time.deltaTime);
+            });
         }
 
-        // Start is called before the first frame update
-        void Start()
+        public void Jump()
         {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
+            if (_controller.isGrounded)
+            {
+                _direction.y = _jumpFactor;
+            }
+            else
+            {
+                Debug.LogWarning("[Player::Jump] Object is not grounded yet.");
+            }
         }
     }
 }
