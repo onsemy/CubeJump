6935a58fe0b3ad8dde896c7c19d36a25f368d823
Jeongjin Oh <onsemy@gmail.com>
2020-01-13 13:50:19 +0900

    코어 로직 제작중
    * GameLogic, Player 스크립트 추가
    * Player용 Material 추가

---
+++ Assets/Scripts/Core/GameLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JJFramework.Runtime.Attribute;
+using JJFramework.Runtime.Extension;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameRuntime.Core.Play
+{
+    public class GameLogic : ExMonoBehaviour
+    {
+        [ComponentPath] protected Text txtScore;
+        [ComponentPath] protected Player objPlayer;
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JJFramework.Runtime.Extension;
+using UnityEngine;
+
+namespace GameRuntime.Core.Play
+{
+    public class Player : ExMonoBehaviour
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
