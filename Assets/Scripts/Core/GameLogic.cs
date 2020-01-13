751b630b65615cfc1e9aad03ea1399faca3ecad3
Jeongjin Oh <onsemy@gmail.com>
2020-01-13 22:31:28 +0900

    n회 플레이 가능하도록 작업
    * 최초 속도 5f부터 시작해서 4f~8f의 임의의 속도로 무한히(?) 진행되도록 작업
    * 랜덤은 시드를 고정해서 일정하게 나오도록 작업
    * 기둥이 Collider Trigger를 통해서 재설정이 되는게 아닌 특정 위치까지 갈 때 재설정 되도록 작업

--- Assets/Scripts/Core/GameLogic.cs
+++ Assets/Scripts/Core/GameLogic.cs
@@ -5,10 +5,12 @@ using JJFramework.Runtime;
 using JJFramework.Runtime.Attribute;
 using JJFramework.Runtime.Extension;
 using UniRx;
+using UniRx.Diagnostics;
 using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace GameRuntime.Core.Play
 {
@@ -16,35 +18,46 @@ namespace GameRuntime.Core.Play
     {
         [ComponentPath, SerializeField] protected Button btnStart;
         [ComponentPath, SerializeField] protected Text txtTime;
+        [ComponentPath, SerializeField] protected Text txtSpeed;
         [ComponentPath, SerializeField] protected Player objPlayer;
         [ComponentPath, SerializeField] protected BoxCollider objCube;
         [ComponentPath, SerializeField] protected BoxCollider objRegenArea;
 
         [SerializeField] private FloatReactiveProperty _time = new FloatReactiveProperty(0f);
+        [SerializeField] private FloatReactiveProperty _speedFactor = new FloatReactiveProperty(5f);
+        [SerializeField] private int _randSeed = 2001132211;
 
         public BoolReactiveProperty IsStart { get; } = new BoolReactiveProperty(false);
 
         protected override void Awake()
         {
             base.Awake();
+            
+            UnityEngine.Random.InitState(_randSeed);
 
             IsStart.Subscribe(x => btnStart.SetActive(!x));
             IsStart.Value = false;
             
             txtTime.text = string.Empty;
+            txtSpeed.text = string.Empty;
 
             objCube.UpdateAsObservable()
                 .Select(x => objCube.transform)
                 .Where(t => IsStart.Value)
-                .Subscribe(t => t.Translate(-Time.deltaTime * 5f, 0f, 0f));
+                .Subscribe(t =>
+                {
+                    t.Translate(-Time.deltaTime * _speedFactor.Value, 0f, 0f);
+                    if (t.localPosition.x < -5f)
+                    {
+                        t.localPosition = new Vector3(4f, -1.5f, 0f);
+                        _speedFactor.Value = UnityEngine.Random.Range(4f, 8f);
+                        Debug.Log($"[GameLogic] Current SpeedFactor: {_speedFactor.Value}");
+                    }
+                });
 
             objCube.OnTriggerEnterAsObservable()
                 .Where(c => c.name == nameof(objPlayer))
                 .Subscribe(c => IsStart.Value = false);
-            
-            objRegenArea.OnTriggerEnterAsObservable()
-                .Where(c => c.name == nameof(objCube))
-                .Subscribe(c => objCube.transform.localPosition = new Vector3(4f, -1.5f, 0f));
 
             Observable.EveryUpdate()
                 .Where(x => IsStart.Value)
@@ -55,6 +68,8 @@ namespace GameRuntime.Core.Play
                 .Where(x => IsStart.Value == false)
                 .Subscribe(_ =>
                 {
+                    UnityEngine.Random.InitState(_randSeed);
+                    _speedFactor.Value = 5f;
                     objCube.transform.localPosition = new Vector3(4f, -1.5f, 0f);                   
                     _time.Value = 0f;
                     IsStart.Value = true;
@@ -64,7 +79,8 @@ namespace GameRuntime.Core.Play
                 .Where(x => IsStart.Value)
                 .Subscribe(_ => _time.Value += Time.deltaTime);
 
-            _time.SubscribeToText(txtTime, x => $"Time: {x:0.0}");
+            _time.SubscribeToText(txtTime, x => $"Time: {x:0.000}");
+            _speedFactor.SubscribeToText(txtSpeed, x => $"Speed: {x:0.000}");
         }
     }
 }
